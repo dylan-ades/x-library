df6ba9e8e900a76d5db77081426d1fb10e400704
GKFitz123 <gkf2103@gmail.com>
2023-04-28 22:54:20 -0400

    connected the setting to dot io, successfully migrated, and running server with no errors

--- x_library/x_library/settings.py
+++ x_library/x_library/settings.py
@@ -74,14 +74,23 @@ WSGI_APPLICATION = 'x_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GKFitz/x_library',
+        'USER': 'GKFitz',
+        'PASSWORD': 'v2_43adc_Dbc8rKbszHTZAe5DjCnNkWV',
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
