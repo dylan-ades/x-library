81aa79b3f9b72c9d4abe70c55a50481334fa2dd3
GKFitz123 <gkf2103@gmail.com>
2023-04-29 03:10:39 -0400

    started a basic workout model to better work on authorization

--- x_library/main_app/models.py
+++ x_library/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
+
+# A tuple of 2-tuples
+TYPE = (
+    ('C', 'Cardio'),
+    ('Y', 'Yoga'),
+    ('S', 'Strength')
+)
+
 
 # Create your models here.
+class Workout(models.Model):
+    type = models.CharField(
+        max_length=1,
+    # add the 'choices' field option
+        choices=TYPE,
+    # set the default value for meal to be 'B'
+        default=TYPE[0][0]
+    )
+    description = models.TextField(max_length=250)
+    
