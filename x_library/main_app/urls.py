c0cafc753ab9566c68a115c5788f6d0bfd9cd497
angel <angelnolasco24@gmail.com>
2023-05-02 15:14:33 -0400

    Exercise confirm delete

--- x_library/main_app/urls.py
+++ x_library/main_app/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
    path('workouts/', views.workouts_index, name='index'),
    # signup
    path('accounts/signup/', views.signup, name='signup'),
-   path('exercises/create/', views.ExerciseCreate.as_view(), name='exercises_create'),
+   # 
+   path('exercises/create/', views.ExercisesCreate.as_view(), name='exercises_create'),
     path('exercises/', views.ExercisesIndex.as_view(), name='exercises_index'),
-    path('exercises/<int:pk>/', views.ExerciseDetail.as_view(), name='exercises_detail'),
-    path('exercises/<int:pk>/update/', views.ExerciseUpdate.as_view(), name='exercises_update'),
-    path('exercises/<int:pk>/delete/', views.ExerciseDelete.as_view(), name='exercises_delete'),
+    path('exercises/<int:pk>/', views.ExercisesDetail.as_view(), name='exercises_detail'),
+    path('exercises/<int:pk>/update/', views.ExercisesUpdate.as_view(), name='exercises_update'),
+    path('exercises/<int:pk>/delete/', views.ExercisesDelete.as_view(), name='exercises_delete'),
 ]
