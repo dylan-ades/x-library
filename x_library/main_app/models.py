53fd5bd8b7484fbb744d78cbbd517776a90a673f
angel <angelnolasco24@gmail.com>
2023-05-01 20:21:45 -0400

    Exercise models

--- x_library/main_app/models.py
+++ x_library/main_app/models.py
@@ -15,4 +15,21 @@ class Workout(models.Model):
     workout_type = models.TextField(max_length=150, default='Workout')
     description = models.TextField(max_length=250)
 
+
+class Exercises(models.Model):
+     name = models.CharField(max_length=100)
+     sets = models.IntegerField()
+     reps = models.IntegerField()
+     distance = models.IntegerField()
+     yoga_flow = models.IntegerField()
+
+     def __str__(self):
+        return f'{self.name}'
+    
+     def get_absolute_url(self):
+        return reverse('exercises_detail', kwargs={'pk': self.id})
+
+
+
+
     
