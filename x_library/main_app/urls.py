03971f4fad9b4e24c86c9be8c9b1ce1c94acd7eb
angel <angelnolasco24@gmail.com>
2023-05-03 14:14:09 -0400

    urls.py

--- x_library/main_app/urls.py
+++ x_library/main_app/urls.py
@@ -7,14 +7,10 @@ urlpatterns = [
    # signup
    path('accounts/signup/', views.signup, name='signup'),
    # 
-   path('exercises/create/', views.ExercisesCreate.as_view(), name='exercises_create'),
-    path('exercises/', views.ExercisesIndex.as_view(), name='exercises_index'),
-    path('exercises/<int:pk>/', views.ExercisesDetail.as_view(), name='exercises_detail'),
-    path('exercises/<int:pk>/update/', views.ExercisesUpdate.as_view(), name='exercises_update'),
-    path('exercises/<int:pk>/delete/', views.ExercisesDelete.as_view(), name='exercises_delete'),
-    path('exercises/create/', views.ExerciseCreate.as_view(), name='exercises_create'),
-    path('exercises/', views.ExerciseIndex.as_view(), name='exercises_index'),
-    path('exercises/<int:pk>/', views.ExerciseDetail.as_view(), name='exercises_detail'),
-    path('exercises/<int:pk>/update/', views.ExerciseUpdate.as_view(), name='exercises_update'),
-    path('exercises/<int:pk>/delete/', views.ExerciseDelete.as_view(), name='exercises_delete'),
+  
+   path('exercises/create/', views.ExerciseCreate.as_view(), name='exercises_create'),
+   path('exercises/', views.ExerciseIndex.as_view(), name='exercises_index'),
+   path('exercises/<int:pk>/', views.ExerciseDetail.as_view(), name='exercises_detail'),
+   path('exercises/<int:pk>/update/', views.ExerciseUpdate.as_view(), name='exercises_update'),
+   path('exercises/<int:pk>/delete/', views.ExerciseDelete.as_view(), name='exercises_delete'),
 ]
